2a2eeed2ebd2ce770a41b8f28eaef7c723500faf
ShaikhAruba <127388919+ShaikhAruba@users.noreply.github.com>
2023-10-04 13:13:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import joblib
 import sklearn
-from sklearn.ensembles import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 # Load the pre-trained Random Forest model
 model = joblib.load("rf.pkl")
